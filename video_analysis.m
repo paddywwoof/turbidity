1ee26f0d7a487b624ca4abbc519e0add725bc252
paddywwoof <patrick@eldwick.org.uk>
2018-12-13 20:50:32 +0000

    extraction of data into arrays and basic plot

--- video_analysis.m
+++ video_analysis.m
@@ -15,10 +15,10 @@ clear all, close all
 pkg load image
 
 VIDEO = 'JRG_run05.avi';   # video of experimental run
-RESOLUTION = 1907 / 1220;  # pixels -> mm
+RESOLUTION = 1000 / 1666;  # mm per pixel
 FPS = 50.0;
 DATA_STEP = 5;             # only create a data point every n video frames
-IMAGE_STEP = 25;           # record image array. NB needs to be a multiple of DATA_STEP
+IMAGE_STEP = 50;           # record image array. NB needs to be a multiple of DATA_STEP
 
 ROW_CROP = 412:711;  # to give clear  background
 COL_CROP = 226:1705; # ditto
@@ -27,7 +27,7 @@ THRESH_R = 25; # these might need some tweaking,
 THRESH_C = 7;  #  also adjust crop ranges to get rid of bits at edges
 
 START_TM = 41; # beginning of interest in s
-STOP_TM = 45;  # end of interst
+STOP_TM = 45;  # end of interst 
 
 video_grab; # load functions in script file_in_loadpath
 start_conversion(VIDEO, START_TM, STOP_TM, FPS); # this runs asynchronously so should keep in front of these calcs
@@ -73,9 +73,27 @@ for i = 1:IMAGE_STEP:n_fr
     hold off
 endfor
 
-# TODO - the posterize and find_edges should be done inside the image loading loop. There
-# probably only needs to be a few images displayed (2 per second?) but the velocity and
-# area of the sediment 'cloud' should be calculated more frequently (5 or 10 times per
-# second?
+# extracting data - probably should save it in this format anyway!
+#
+tm = []; top = []; front = []; area = []; frame = [];
+for data_rec = data
+  d = data_rec{1};
+  if not(isempty(d))
+    tm(end + 1) = d.tm;
+    top(end + 1) = d.top * RESOLUTION;
+    front(end + 1) = d.front * RESOLUTION;
+    area(end + 1) = d.area * (RESOLUTION ^ 2);
+    frame(end + 1) = d.frame;
+  endif
+endfor
 
-# TODO 2 - in find_edges the areas of the different colours should also be returned
+# produce plot of changes in top, front, area
+plot(tm, top);
+hold on;
+plot(tm, front);
+plot(tm, area / 100);
+plot(tm, front ./ tm);
+xlabel('seconds');
+legend('top mm', 'front mm', 'area mm^2 / 10', 'velocity mm/s');
+title('Turbidity change over time');
+hold off;
