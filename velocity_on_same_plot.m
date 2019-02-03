9984a45733620e3f3994ebc3d47acd81477cc0ac
paddywwoof <patrick@eldwick.org.uk>
2019-02-03 00:02:39 +0000

    some error calculation

--- velocity_on_same_plot.m
+++ velocity_on_same_plot.m
@@ -10,6 +10,10 @@ BKPS = {{4, 263.9, 19.6, 11, 103}, # the number of video i.e. ..h00_r04... padde
         {9, 262.3, 140.6, 25, -1},
         {10, 262.3, 144.5, 20, -1}};
 key = char(zeros(1, 5)); #vid04 etc
+
+x_vals = [];
+v_vals = [];
+
 figure
 hold on
 for n = 1:length(BKPS)
@@ -19,10 +23,38 @@ for n = 1:length(BKPS)
   velocities = (front(2:end, 6) - front(1:end-1, 6))' ./ (tm(2:end) - tm(1:end-1));
   velocities = max(velocities, 0.0); # get rid of initial negative velocities
   sm = smooth(velocities, 0.1); # exponential smoothing with factor of 0.1
-  plot(front(2:end, 6)' - BKPS{n}{3}, sm);
+  x = front(2:end, 6)' - BKPS{n}{3};
+  plot(x, sm); # NB  smoothed velocity against distance
   xlim([0.0, 800.0]);
   ylim([0.0, 300.0]);
+  
+  if n != 1 & n != 3 # these look dodgy results
+    x_vals = [x_vals, x]; # this is how to concatenate arrays in matlabroot
+    v_vals = [v_vals, sm];
+  endif
   #
 endfor
 legend(key);
+
+# now find best straight line for first part of figure (need to do second one too)
+ix = find(x_vals >= 0.0 & x_vals <= 400.0); # index to values of dist <= 400
+[p, S] = polyfit(x_vals(ix), v_vals(ix), 1); # polynomial order 1 -> strt line
+[v_fit, delta] = polyval(p, x_vals(ix), S);
+# delta is estimate of std error at values of x. i.e. 95% error bars would be
+# +/- 2 * delta
+printf(' error +/- %d', 2 * delta(1))
+plot(x_vals(ix), v_fit, 'r-');
+plot(x_vals(ix), v_fit + 2 * delta, 'm--', x_vals(ix), v_fit - 2 * delta, 'm--')
+#
+# and second part...
+ix = find(x_vals > 400.0 & x_vals <= 750.0); # index to values of dist > 400
+[p, S] = polyfit(x_vals(ix), v_vals(ix), 1); # polynomial order 1 -> strt line
+[v_fit, delta] = polyval(p, x_vals(ix), S);
+# delta is estimate of std error at values of x. i.e. 95% error bars would be
+# +/- 2 * delta
+printf(' error +/- %d', 2 * delta(1))
+plot(x_vals(ix), v_fit, 'r-');
+plot(x_vals(ix), v_fit + 2 * delta, 'm--', x_vals(ix), v_fit - 2 * delta, 'm--')
+
+# or fit a curve to the data - need to think about the likely physics
 hold off
