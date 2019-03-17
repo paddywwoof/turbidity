18200708deebe9c22b132270f7b8829c5756d2ca
JillGaunt <jill@eldwick.org.uk>
2019-03-17 23:17:42 +0000

    comparing on the same plot

---
+++ UDV/velocity_profiles_on_same_plot.m
@@ -0,0 +1,21 @@
+FILES = {'UDV_velocity_profllie_15cm.csv','UDV_velocity_profllie_51cm.csv','UDV_velocity_profllie_90cm.csv'};
+
+COLRS = ['r', 'b','m'];
+useful_functions;
+
+height = [0.0, 10.0, 40.0, 70.0, 100.0];
+
+
+mean_vt = {};
+for i = 1:length(FILES);
+  csv_data = csvread(FILES{i});
+  key = COLRS(i)
+  mean_vt{i} = csv_data(1,:);
+  plot(mean_vt, height)
+endfor
+#{
+xlabel('U (mm s^{-1})')
+ylabel('Height (mm)')
+legend('15cm','55cm', '86cm')
+title('Velocity profiles with varying distance down stream')
+holdoff
