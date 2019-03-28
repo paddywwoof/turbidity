5dedf44a15fa44b415194cbab66e2f7df1a90f7f
JillGaunt <jill@eldwick.org.uk>
2019-03-28 22:49:29 +0000

    whoops

---
+++ UDV/colourmap.m
@@ -0,0 +1,12 @@
+udv_data = csvread('JILL_027.csv')*-1;
+
+#imagesc(udv_data)
+#contour(udv_data)
+#contourf(udv_data)
+#pcolor(udv_data)
+#surf(udv_data, 'edgecolor', 'none'); view(2);
+
+udv_data = min(max(udv_data, -50),200); #sets range between 200 and -50 (gets rid of outliers)
+imagesc([2.0, 93.98],[66.6, 0], udv_data)
+xlabel('mm')
+ylabel('s')
