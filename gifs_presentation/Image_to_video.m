69ddd403ef77be48f2c09dae97b31bf7ae7050c2
JillGaunt <jill@eldwick.org.uk>
2019-04-18 10:37:39 +0100

    making videos

---
+++ gifs_presentation/Image_to_video.m
@@ -0,0 +1,18 @@
+pkg load image
+
+#
+pics = dir('*.jpg'); #finds jpeg files in folder 026
+pics = {pics.name}';
+#}
+
+
+
+v = VideoWriter('unobstructed_concentration_run.avi');
+v.FrameRate = 0.5;
+open(v)
+
+for i = 1:length(pics)
+  img = imread(pics{i});
+  writeVideo(v,img)
+endfor
+close(v)
