643c5393127bb4de04289cde3976e6ea07e5abdc
paddywwoof <patrick@eldwick.org.uk>
2019-05-04 22:28:55 +0100

    axis font tick scaling patches

--- UDV/Fr_and_Re_graphs.m
+++ UDV/Fr_and_Re_graphs.m
@@ -1,5 +1,6 @@
 pkg load image;
-#useful_functions;
+addpath('../');
+useful_functions;
 
 FILE = csvread('Fr_and_Re.csv');
 
@@ -12,24 +13,26 @@ ReO = FILE(6:10,3);
 H = FILE(6:10,5);
 Hun = FILE(6,5);
 #
-f = figure;
+f1 = figure;
 subplot(1,2,1);
 plot(FrO, H, 'xk', 'markersize', 10, FrUO, Hun, '.b', 'markersize', 20);
 xlabel('Froude number', 'fontsize', 16);
 ylabel('Zo/Zf', 'fontsize', 16);
 
-f = get(gcf,'currentaxes');
-set(f, 'fontsize', 16);
+xticklabels = [0, 0.4, 0.8, 1.2];
+xtick = xticklabels + 0.05;
+set(gca, 'XTick', xtick, 'fontsize', 16, 'XTickLabel', xticklabels, 'fontsize', 16);
 
 subplot(1,2,2);
 plot(ReO, H, 'xk', 'markersize', 10, ReUO, Hun, '.b', 'markersize', 20);
 xlabel('Reynolds Number', 'fontsize', 16);
 leg = legend('Obstructed','Unobstructed');
-set(leg, 'fontsize', 16);
-legend boxoff
+set(leg, 'fontsize', 12);
+legend('boxoff');
 
-f = get(gcf,'currentaxes');
-set(f, 'fontsize', 16);
+xticklabels = [0, 2000, 4000, 6000];
+xtick = xticklabels + 5;
+set(gca, 'XTick', xtick, 'fontsize', 16, 'XTickLabel', xticklabels, 'fontsize', 16);
 
-figure_size (f, 'talk size froude and rerynolds numbers.jpeg', 10,15)
+figure_size (f1, 'talk size froude and rerynolds numbers.jpeg', 10, 15)
 #saveas(fig,'Froude and Reynods numbers.jpeg');
